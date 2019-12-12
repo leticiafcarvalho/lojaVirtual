00059f0bb27671a9f12c24de9f5f5949a96ef3de
Leticia Franco <leticia.franco@FITCARD.infra.br>
2019-12-12 14:51:56 -0300

    first commit

---
+++ LojaVirtual/Controllers/ProdutoController.cs
@@ -0,0 +1,34 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            Produto produto = GetProduto();
+
+            return View(produto);
+
+            //return new ContentResult() {Content = "<h3> Produto -> Visualizar </h3>", ContentType = "text/html"};
+            //return "Produto -> Visualizar";
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox One X",
+                Descricao = "Jogue em 4k",
+                Valor = 2000.00M
+
+            };
+        }
+    }
+}
