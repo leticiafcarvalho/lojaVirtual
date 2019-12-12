8cdd2238265dd3f22125ee912f40c51279bfd60b
Leticia Franco <leticia.franco@FITCARD.infra.br>
2019-12-12 17:12:38 -0300

    Action

--- LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/Controllers/HomeController.cs
@@ -12,5 +12,35 @@ namespace LojaVirtual.Controllers
         {
             return View();
         }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            //Contato contato = new Contato();
+            contato.Nome = HttpContext.Request.Form["nome"];
+            contato.Email = HttpContext.Request.Form["email"];
+            contato.Texto = HttpContext.Request.Form["texto"];
+
+            return new ContentResult() { Content = string.Format("Dados recebidos com sucesso!<br/> Nome: {0} <br/>E-mail: {1} <br/>Texto: {2}", contato.Nome, contato.Email, contato.Texto), ContentType = "text/html" };
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
     }
 }
