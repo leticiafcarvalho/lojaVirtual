ebc203cd854f5b64f720c973819f22c6187005b4
leticia.franco <leticia.franco@TILETICIA>
2019-12-13 14:48:59 -0300

    Email enviar

--- LojaVirtual/Libraries/Email/ContatoEmail.cs
+++ LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -10,32 +10,44 @@ namespace LojaVirtual.Libraries.Email
 {
     public class ContatoEmail
     {
+        public static NetworkCredential Credentials { get; private set; }
+
         public static void EnviarContatoPorEmail(Contato contato)
         {
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            /*SmtpClient smtp = new SmtpClient("smtp.office365.com", 587);
             smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new NetworkCredential("leticia.fcarvalho.job@gmail.com", "32452042");
-            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential("leticia.fcarvalho.job@gmail.com", "senha");
+            smtp.EnableSsl = true;*/
+
+
+            SmtpClient smtp = new SmtpClient
+            {
+                Port = 587,
+                Host = "smtp.office365.com",
+                EnableSsl = true,
+                //client.Timeout = 10000;
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("naoresponda@primebeneficios.com.br", "Ruta6637")
+            };
 
             string corpoMsg = string.Format("<h2>Contato - LojaVirtual</h2>" +
-                "<br>Nome: </b> {0} <br />" +
-                "<b>Email: </b> {1} <br />" +
+                "<b>Nome: </b> {0} <br />" +
+                "<b>E-mail: </b> {1} <br />" +
                 "<b>Texto: </b> {2} <br />" +
-                "<br /> <b>E-mail enviado automaticamente da LojaVirtual.</b>", 
+                "<br /> E-mail enviado automaticamente do site LojaVirtual.",
                 contato.Nome,
-                contato.Email, 
+                contato.Email,
                 contato.Texto
-            );    
+            );
 
             MailMessage mensagem = new MailMessage();
-            mensagem.From = new MailAddress("leticia.fcarvalho.job@gmail.com");
+            mensagem.From = new MailAddress("naoresponda@primebeneficios.com.br");
             mensagem.To.Add("leticia.fcarvalho.job@gmail.com");
             mensagem.Subject = "Contato - LojaVirtual - E-mail: " + contato.Email;
-            mensagem.Body = "Texto: " + contato.Texto;
             mensagem.IsBodyHtml = true;
 
             smtp.Send(mensagem);
- 
         }
     }
 }
